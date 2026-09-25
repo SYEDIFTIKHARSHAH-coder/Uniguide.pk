10759a96f078979ccc90107f7f88ec579d51cef7
SYEDIFTIKHARSHAH-coder <ssyedifitkharshah49@gmail.com>
2026-09-25 16:50:42 +0500

    Remove localhost from CORS for production

--- server/server.js
+++ server/server.js
@@ -58,6 +58,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5173",
   "http://localhost:3000",
   "http://127.0.0.1:3000",
+  "https://uniguid-pk.web.app",
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
